f6f1c8f9c1716658671114173fac0b47f2968396
anmedev <aam2ds@virginia.edu>
2023-10-26 17:04:14 -0400

    add comments and shortened project 4's description

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const express = require("express");
-const app = express();
+/* Treehouse FSJS Techdegree
+FSJS Project 6 - Static Node.js and Express Site
+*/
+
+// Imports Express.
+const app = require("express")();
 // const data = require();
+
+// Sets the Pug template as the template engine to use.
 app.set("view engine", "pug");
+
+// Turns on Express server.
 app.listen(3000, () => {
   console.log("The application is running on localhost:3000!")
 });
