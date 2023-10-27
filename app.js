21b5ff8c24dcff9996b334707b6f5bbcf8f38bf4
anmedev <aam2ds@virginia.edu>
2023-10-27 07:27:21 -0400

    move routes from this file to the routes folder

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ FSJS Project 6 - Static Node.js and Express Site
 */
 
 // Imports Express.
-const app = require("express")();
-// const data = require();
+const express = require("express")
+const app = express();
 
 // Sets the Pug template as the template engine to use.
 app.set("view engine", "pug");
 
-// Turns on Express server.
+// Serves the static files in the public folder.
+app.use(express.static("public"));
+
+// Turns on the Express server.
 app.listen(3000, () => {
   console.log("The application is running on localhost:3000!")
 });
