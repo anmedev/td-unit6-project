530e8ec9e10a2d72b3d8b479f474954eee6f9b71
anmedev <aam2ds@virginia.edu>
2023-10-27 07:29:08 -0400

    create routes file and add routes for each page

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+// Imports the express application.
+const express = require("express");
+// Creates a new Router obj.
+const router = express.Router();
+// Provides access to the JSON data.
+const { projects } = require("../data.json");
+
+// Creates the route for the Home page.
+router.get("/", (req, res) => {
+  res.render("index", {projects});
+});
+
+// Creates the route for the About page.
+router.get("/about", (req, res) => {
+  res.render("about");
+});
+
+// Creates the route for each project page.
+router.get("/projects/:id", (req, res) => {
+  const projectId = req.params.id;
+  const project = projects.find(({ id }) => id === +projectId);
+  res.render("project", {project});
+});
+
+// Imports the router.
+module.exports =  router;
